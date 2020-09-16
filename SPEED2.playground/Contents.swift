80df7b6535bacfdde5730adc0aedfee07e18fbaa
mariam alfoudari <maf.alfoudari@gmail.com>
2020-09-16 17:29:06 +0300

    done

--- SPEED2.playground/Contents.swift
+++ SPEED2.playground/Contents.swift
@@ -9,10 +9,19 @@
 */
 var numbers = [1,2,3,4,5,6,7,8,9,10]
 
-
+for _ in 1...10 {
+    print(numbers)
+    
+}
 
 /*:
 
 # BONUS🎁 قم بكتابه كود يقوم بطباعة الأرقام الزوجية فقط 
 */
-
+var evenNumbers: [Int]=[]
+for number in numbers{
+    if number % 2 == 0{
+        evenNumbers.append(number)
+    }
+}
+print(evenNumbers)
